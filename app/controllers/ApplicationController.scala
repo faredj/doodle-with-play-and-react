8263d5e2d8b189bb87e3e16d205f0c556c910649
fhammache <faredj.hammache@particeep.com>
2021-03-13 19:20:26 +0100

    init connection pages

---
+++ app/controllers/ApplicationController.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class ApplicationController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+
+  def register() = Action { implicit request: Request[AnyContent] =>
+    Ok("OK")
+  }
+}
--- front-app/src/App.js
+++ front-app/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {BrowserRouter, Switch, Route, Redirect} from "react-router-dom";
+import Register from "./pages/connection/register/Register";
+import Login from "./pages/connection/login/Login";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path={"/register"} component={Register} exact={true}/>
+                <Route path={"/login"} component={Login} exact={true}/>
+                <Redirect exact from="/" to="/login"/>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
